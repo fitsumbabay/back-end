e136cad4a521b9ee4f32727860e54433c6424fdf
fitsumbabay <fitsumbabay@gmail.com>
2024-12-28 19:41:04 -0500

    added odels routes and server

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const itemRoutes = require("./routes/itemRoutes");
+const weatherRoutes = require("./routes/weatherRoutes");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.use("/api/items", itemRoutes); //  item routes
+app.use("/api", weatherRoutes); //  weather routes
+
+app.get("/", (req, res) => {
+  res.send(" Greetings from the backend! ☀️");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const crypto = require("crypto");
+const secret = crypto.randomBytes(64).toString("hex");
+console.log(secret);
+
